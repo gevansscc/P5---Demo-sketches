1560a87fe115dd94849a3a95ca9d9c5ba77f49bb
gevansscc <76914070+gevansscc@users.noreply.github.com>
2021-01-14 13:01:13 -0500

    Revert "Revert to old"
    This reverts commit a7dc25c78092fcf247f3aae8b9dfa81765b8c4b0.

--- week1/sketch.js
+++ week1/sketch.js
@@ -4,13 +4,9 @@ function setup() {
 
 function draw() {
 background(220); 
-<<<<<<< HEAD
 ellipse(width/2, height/2, 20,20);
 ellipse(width/2, height/2, 20,20);
 ellipse(width/2, height/2, 20,20);
 ellipse(width/2, height/2, 20,20);
 ellipse(width/2, height/2, 20,20);
-=======
-ellipse(width/2, height/2, 20,20;
->>>>>>> parent of 5ba1358... updated to sketch
 }
