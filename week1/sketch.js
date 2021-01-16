36966ba07f969fe1c46e9fb52378ccb1cb93c50f
gevansscc <76914070+gevansscc@users.noreply.github.com>
2021-01-16 10:26:45 -0500

    Update sketch.js
    trying somehting out

--- week1/sketch.js
+++ week1/sketch.js
@@ -1,8 +1,26 @@
-function setup() {
-  createCanvas(400, 400);
+let x,y;
+
+function setup(){
+  createCanvas(400,400);
+  cx = width/2;
+  cy = height/2;
+  y = 0;
+  rectMode(CENTER); 
 }
 
-function draw() {
-background(220); 
-ellipse(width/2, height/2, 20,20);
+function draw(){
+ background(155);
+ translate(cx,cy);
+ ellipse(0,y,20,20);
+
+ y += 1;
+
+ if (y > height){
+  y = y*-1;
+ }
+
+//  translate(x,y);
+
+console.log(y);
+
 }
